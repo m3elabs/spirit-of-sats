672932c0d6d5e06a43118695bd658f153cbe84ed
Zach Miller <m3.elabs@gmail.com>
2023-05-16 17:44:12 -0400

    responseive teset

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import  "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
       return (
+        <>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
             <Component {...pageProps} />
+            </>
       );
     }
