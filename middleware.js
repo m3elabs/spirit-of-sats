6e0f2bd3d9027d238171545c675696c8ee80bfb1
Zach Miller <m3.elabs@gmail.com>
2023-05-17 16:49:55 -0400

    middleware

--- middleware.js
+++ middleware.js
@@ -3,17 +3,18 @@ import { NextResponse } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const token = request.cookies.get('token')
-  if (request.url === "http://localhost:3000/" && token?.value !== "" && token?.value !== undefined ) {
-    return NextResponse.redirect(new URL("/chat", request.url));
-  }
-  if (request.url === "http://localhost:3000/chat") {
-    if (token?.value === "" || token?.value === undefined) {
-      return NextResponse.redirect(new URL("/", request.url));
-    } else {
-        return NextResponse.next();
-    }
+  console.log(request.url)
+//   if (request.url === "http://localhost:3000/" && token?.value !== "" && token?.value !== undefined ) {
+//     return NextResponse.redirect(new URL("/chat", request.url));
+//   }
+//   if (request.url === "http://localhost:3000/chat") {
+//     if (token?.value === "" || token?.value === undefined) {
+//       return NextResponse.redirect(new URL("/", request.url));
+//     } else {
+//         return NextResponse.next();
+//     }
  
-  }
+//   }
 
 
   if (request.url === "https://spirit-0f-satoshi.vercel.app/" && token?.value !== "" && token?.value !== undefined ) {
